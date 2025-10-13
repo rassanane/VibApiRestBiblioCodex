f7cafbb5321b13f6d8c53f4e802edd9f2c6f9ee5
rassanane <rassanane@gmail.com>
2025-10-13 11:41:42 +0200

    Test

--- src/main/java/fr/keltou/biblio/BiblioApplication.java
+++ src/main/java/fr/keltou/biblio/BiblioApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /*
-Mon application de test 13/10/2025
+Mon application de test 13/10/2025 -2
 */
 @SpringBootApplication
 public class BiblioApplication {
